f0a083ba51f9ac56784f704ce63eb8c1dea10a10
wagoid <w.rsantos@outlook.com>
2017-11-11 19:15:34 -0200

    Update controllers logic

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -1,20 +1,42 @@
-class Api::ClientsController < ApplicationController
+module Api
+  class ClientsController < ApplicationController
+    before_action :set_client, only: %i[show update destroy]
 
     def index
-        render json: { clients: Client.all }
+      render json: { clients: Client.all }
     end
 
     def show
-        render json: { client: Client.find(params[:id]) }
+      render_show_client
     end
 
     def create
+      render json: { client: Client.create(client_params) }
     end
 
     def update
+      if @client.update(client_params)
+        render_show_client
+      else
+        render_validation_errors(@client)
+      end
     end
 
     def destroy
+      @client.destroy!
+      render body: nil, status: :no_content
     end
 
+    def set_client
+      @client = Client.find(params[:id])
+    end
+
+    def render_show_client
+      render json: { client: @client }
+    end
+
+    def client_params
+      params.require(:client).permit(:name, :registration, :email, :zip_code, :address, :city, :state, :country)
+    end
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   def index
     render file: 'public/index.html'
   end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :response_for_record_not_found
+
+  def response_for_record_not_found(ex)
+    render json: { errors: [I18n.t('record_not_found'), ex.message] }, status: :not_found
+  end
+
+  def render_validation_errors(model)
+    render json: { errors: model.errors }, status: :unprocessable_entity
+  end
 end
