3fce82b3a7fc105a536d1d416c45287b40f74b0d
wagoid <w.rsantos@outlook.com>
2017-11-22 23:41:55 -0200

    Add price column to products

--- app/serializers/input/product_serializer.rb
+++ app/serializers/input/product_serializer.rb
@@ -1,8 +1,10 @@
 # @attr [string] id
 # @attr [string] name
 # @attr [string] description
+# @attr [float] price
 class Input::ProductSerializer < ActiveModel::Serializer
   attribute :id
   attribute :name
   attribute :description
+  attribute :price
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,12 +1,14 @@
 # @attr [string] id
 # @attr [string] name
-# @attr [string] instalments
+# @attr [string] description
+# @attr [float] price
 # @attr [date-time] created_at
 # @attr [date-time] updated_at
 class ProductSerializer < ActiveModel::Serializer
   attribute :id
   attribute :name
   attribute :description
+  attribute :price
   attribute :created_at
   attribute :updated_at
 end
